cf573420c0b91520e515f6472d4731ec0a0181a0
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-02-08 11:02:41 +0100

    feat: add bpmn viewer

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+
+import '@expo/browser-polyfill';
+
+import BpmnViewer from './BpmnViewer';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={ styles.container }>
+      <BpmnViewer
+        url="https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn" />
       <StatusBar style="auto" />
     </View>
   );
